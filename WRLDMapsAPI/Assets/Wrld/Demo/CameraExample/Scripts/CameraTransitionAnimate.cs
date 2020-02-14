c5ecb1b2a035c83fc8bc6ea0a64f246754783c90
Vaibhav Rakheja <vaibhav@Vaibhavs-MacBook-Pro.local>
2020-02-13 21:27:36 -0700

    Code files pushed

---
+++ WRLDMapsAPI/Assets/Wrld/Demo/CameraExample/Scripts/CameraTransitionAnimate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using Wrld;
+using Wrld.Space;
+using UnityEngine;
+
+public class CameraTransitionAnimate : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        StartCoroutine(Example());
+    }
+
+    IEnumerator Example()
+    {
+        //var startLocation = LatLong.FromDegrees(37.7858, -122.401);
+        var startLocation = LatLong.FromDegrees(53.495, -113.5210);
+        Api.Instance.CameraApi.MoveTo(startLocation, distanceFromInterest: 1890);
+
+        yield return new WaitForSeconds(4.0f);
+
+        //var destLocation = LatLong.FromDegrees(37.802, -122.4058);
+        var destLocation = LatLong.FromDegrees(53.515, -113.5276);
+        Api.Instance.CameraApi.AnimateTo(destLocation, distanceFromInterest: 500, headingDegrees: 270, tiltDegrees: 30, transitionDuration: 5, jumpIfFarAway: false);
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+}
+
---
+++ WRLDMapsAPI/Assets/Wrld/Demo/CameraExample/Scripts/CameraTransitionMovingCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using Wrld;
+using Wrld.Space;
+using UnityEngine;
+
+public class CameraTransitionMovingCamera : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        StartCoroutine(Example());
+    }
+
+    IEnumerator Example()
+    {
+        var startLocation = LatLong.FromDegrees(53.495, -113.5210);
+        Api.Instance.CameraApi.MoveTo(startLocation, distanceFromInterest: 800, headingDegrees: 0, tiltDegrees: 50);
+
+        yield return new WaitForSeconds(4.0f);
+
+        var destLocation = LatLong.FromDegrees(53.515, -113.5276);
+        Api.Instance.CameraApi.MoveTo(destLocation, distanceFromInterest: 500);
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+}
